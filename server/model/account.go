26e4a9652f6dd1da7150ff74adb7deaa7e05c618
Pablo Heredia <crowl@mm.st>
2016-06-09 16:53:08 -0400

    refactor account model, drop bson dependency, add password hashing and compare methods

--- server/model/account.go
+++ server/model/account.go
@@ -3,16 +3,31 @@ package model
 import (
 	"time"
 
-	"gopkg.in/mgo.v2/bson"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Account struct {
-	ID        bson.ObjectId `bson:"_id"       json:"id"`
-	Role      string        `bson:"role"      json:"role"`
-	Email     string        `bson:"email"     json:"email"`
-	Password  string        `bson:"password"  json:"password"`
-	FirstName string        `bson:"firstName" json:"firstName"`
-	LastName  string        `bson:"lastName"  json:"lastName"`
-	Active    bool          `bson:"active"    json:"active"`
-	CreatedOn time.Time     `bson:"createdOn" json:"createdOn"`
+	ID        int64     `json:"id"`
+	Role      string    `json:"role"`
+	Verified  bool      `json:"verified"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	CreatedOn time.Time `json:"createdOn"`
+	UpdatedOn time.Time `json:"updatedOn"`
+}
+
+func (a *Account) HashPassword() error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(a.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	a.Password = string(hash)
+}
+
+func (a *Account) ComparePassword(password string) error {
+	if err := bcrypt.CompareHashAndPassword([]byte(a.Password), []byte(password)); err != nil {
+		return err
+	}
 }
