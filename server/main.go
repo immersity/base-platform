70e9892231dd4e77da7f10720e6b9f65f37b3f64
Pablo Heredia <crowl@mm.st>
2016-06-09 17:50:24 -0400

    add proper server shutdown using manners package

--- server/main.go
+++ server/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
+	"github.com/braintree/manners"
 	flag "github.com/ianschenck/envflag"
 	"github.com/immersity/base-platform/server/service"
 	"github.com/immersity/base-platform/server/store"
@@ -13,12 +15,13 @@ import (
 
 func main() {
 	var (
-		port      = flag.Int("PORT", 3000, "port")
+		httpAddr  = flag.String("HTTP_ADDRESS", "0.0.0.0:3000", "http address")
 		mysqlDsn  = flag.String("MYSQL_DSN", "crowl:crowl@/immersity", "mysql dsn")
 		jwtSecret = flag.String("JWT_SECRET", "olakease", "jwt secret")
 		jwtExpiry = flag.Duration("JWT_EXPIRY", time.Hour*1, "jwt expiry")
 	)
 	flag.Parse()
+
 	st, err := store.New(store.Config{
 		Dsn: *mysqlDsn,
 	})
@@ -26,9 +29,35 @@ func main() {
 		log.Fatal(err)
 	}
 	defer st.Close()
-	srvc := service.New(st, service.Config{
+
+	s := service.New(st, service.Config{
 		JwtSecret: *jwtSecret,
 		JwtExpiry: *jwtExpiry,
 	})
-	log.Fatal(http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", *port), srvc.Handler()))
+
+	errChan := make(chan error, 1)
+	httpServer := manners.NewServer()
+	httpServer.Addr = *httpAddr
+	httpServer.Handler = s.Handler()
+
+	go func() {
+		errChan <- httpServer.ListenAndServe()
+	}()
+
+	log.Printf("HTTP service listening on %s...\n", *httpAddr)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	for {
+		select {
+		case err := <-errChan:
+			if err != nil {
+				log.Fatal(err)
+			}
+		case sig := <-signalChan:
+			log.Printf("Captured %v. Exiting...\n", sig)
+			httpServer.BlockingClose()
+			os.Exit(0)
+		}
+	}
 }
